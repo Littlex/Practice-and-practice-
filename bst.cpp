f8452cfcbd06fe8e7f9802d4b49c18085d92df54
littlex <sxzhuse@gmail.com>
2018-02-12 17:14:47 -0600

    finish binary search tree

--- bst.cpp
+++ bst.cpp
@@ -2,44 +2,58 @@
 using namespace std;
 
 //define binary tree
-
 struct Node{
 
 int data;
 Node * left;
 Node * right;
 
-
 };
 
 //initialize node 
-
 Node * newNode(int var){
-
 Node * t = new Node;
 t-> data = var;
 t-> left = NULL;
 t -> right = NULL;
-
 return t;
-
 }
 
 
-/*
+/*********************** recursive binary search tree  ******************/
+bool recurBst(struct Node* root, int key){
 
-struct Node * bst(struct Node* root, int data){
+if(root == NULL ) return false;
+if(root -> data == key ) return true;
+else if (root -> data > key) return recurBst(root -> left,key);
+else return recurBst(root -> right, key);
+}//recursive search binary search tree
+/*************************************************************/
 
-Node * current = root;
-    if(root -> data == data || current == NULL) return current;
-    
-   
-   if(root -> data > data) return bst(current->left,data);
-   if(root -> data < data) return bst(current->right,data);
 
-}// binary search tree
-*/
+/************************* iterative search tree *****************/
+bool interBst(struct Node* root, int key){
 
+if(root == NULL ) return 0;
+
+while (root){
+
+if (root -> data == key) return true;
+else if(root -> data > key) root = root -> left;
+else if (root -> data < key) root = root -> right;
+
+}// while
+
+return false;
+
+
+}// interBst(){}
+/*****************************************************************/
+
+
+
+
+/*********************** inorder print a tree ******************/
 void print(Node * root){
 
 if(root == NULL) return ;
@@ -48,9 +62,13 @@ cout << root->data << '\t';
 print (root -> right);
 
 } // print()
+/***************************************************************/
 
-/******************************************************/
-// insert data into a binary tree 
+
+
+
+
+/******************** insert data into a binary tree *****************/
 void insertNode(struct Node* &root, int var){
 
 Node * current = root;
@@ -92,7 +110,9 @@ else {temp -> right = newNode(var);}
 
 
 } // void insertNode(int var)
-/******************************************************/
+/***********************************************************************/
+
+
 
 
 /******************** recursive insert **********************************/
@@ -107,28 +127,142 @@ if(var < root->data) recurInsert(root->left, var);
 else recurInsert(root->right, var);
 
 }// recurInsert()
-/******************************************************/
+/**********************************************************************/
 
 
 
 
 
+/***************** Delete a Node *************************************/
+struct Node * deleteNode (struct Node * &root, int key){
+
+struct Node * pre = NULL;
+struct Node * current = root;
+if(root == NULL) return NULL;
 
 
+while(current){
+
+if(current -> data == key){
+
+//case 1: there is no child of that node 
+if(current -> left == NULL && current -> right == NULL){
+
+if(current == root) {
+delete current; 
+return NULL;
+}
+
+else{
+delete current;
+current = NULL;
+if(pre -> data < key) pre -> right = NULL;
+else pre -> left = NULL;
+return root;
+}// else
+
+}// if
 
 
+//case 2: there is only one child(left or right) of that node
+if(current -> left != NULL && current -> right == NULL){
+// if delete the root node
+if(current == root) {
+root = current -> left;
+delete current;
+return root;
+}
+
+else{
+pre -> left = current -> left;
+delete current;
+current = NULL;
+return root;
+}//else
+}// if
+
+if(current -> left == NULL && current -> right != NULL){
+if(current == root) {
+root = current -> right;
+delete current;
+return root;
+}
+else{
+pre -> right = current -> right;
+delete current;
+current = NULL;
+return root;
+}//else
+}//if
 
 
+// case 3: this node has two children
+if(current -> left !=  NULL && current -> right != NULL){
 
-/******************************************************/
-void deleteNode (int var){
+  
+    // find the maxLeft as the new root Node 
+    Node * temp = current -> left;
+    Node * preTemp = current;
+    
+    // if the right node of current -> left is null
+    if(!temp -> right) {
+    
+    current -> data = temp -> data;
+    current -> left = temp -> left;
+    delete temp;
+    return root;
+    
+    
+    }// if(!temp -> right)
+    
+   
+    
+    while(temp->right){
+    preTemp = temp;
+    temp = temp -> right;
+    }
+    
+    
+    current -> data = temp -> data;
+    delete temp;
+    preTemp -> right = NULL;
+    
+    return root ;
+    
+    // find the minRight as the new root Node 
 
-
-
-
-
+/*                   8 
+                    / \
+                   5   10
+                  / \  / \           
+                 3   6 9  11
+                / \          \
+               1   4          13
+                   /\
+*/
 
 }
+
+
+
+}// if (root->data == key)
+
+else if (current -> data > key){ 
+pre = current; 
+current = current -> left;}
+
+else { 
+pre = current; 
+current = current -> right;}
+}//while
+
+
+// dot not find the key that need to be deleted 
+if(current == NULL)
+cout << "do not find the node with the key that need to be deleted \n";
+return root;
+}// deleteNode()
+/***********************************************************************/
 
 
 int main(){
@@ -150,9 +284,28 @@ root -> right ->right =  newNode(11);
                    5   10
                   / \  / \           
                  3   6 9  11
+                /           \
+               1             13
 */
 
 insertNode(root,8);
+insertNode(root, 5);
+insertNode(root,10);
+insertNode(root,11);
+insertNode(root, 6);
+insertNode(root, 3);
+insertNode(root, 9);
+insertNode(root, 1);
+insertNode(root, 13);
+insertNode(root, 4);
+
+
+cout << "before delete: \n";
+print(root);
+cout << "\nafter delete: \n";
+root = deleteNode(root,99);
+
+
 
 //insertNode(root, 4);
 //insertNode(root,2);
